494cd998b55e73b00ea39d05cff26efb47af1d47
Danny <dannylockhart91@gmail.com>
2019-11-13 15:44:31 +0000

    - Added Header to application - Imported SVG for header logo - Moved body styling into app.scss instead of index.scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.scss';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 const App: React.FC = () => {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route exact path='/shop' component={ShopPage}/>
